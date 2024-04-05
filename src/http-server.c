31cb5933f74ae7e762391a2808a55ef8498346f5
flafflar <achmichail@gmail.com>
2024-04-05 11:57:36 +0300

    feat: replace the old http server with a mongoose https server

--- src/http-server.c
+++ src/http-server.c
@@ -23,65 +23,51 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 #include <arpa/inet.h>
 #include <pthread.h>
 
+#include "mongoose.h"
+
 #include <obs-module.h>
 #include "plugin-support.h"
 
-#define READ_BUFFER_SIZE 4096
-
 struct http_server {
-    int socket_fd;
+    struct mg_mgr mgr;
     pthread_t listen_thread;
-    char *html;
     char *ws_port;
 };
 
-char* http_server_read_html() {
-    char *path = obs_module_file("client/index.html");
+static void http_server_handler(
+    struct mg_connection *conn,
+    int ev,
+    void *data
+) {
+    struct http_server *server = (struct http_server *) conn->mgr->userdata;
 
-    FILE *file = fopen(path, "r");
+    if (ev == MG_EV_ACCEPT) {
+        struct mg_tls_opts opts = {
+            .cert = mg_file_read(&mg_fs_posix, obs_module_file("cert.pem")),
+            .key = mg_file_read(&mg_fs_posix, obs_module_file("key.pem")),
+        };
+        mg_tls_init(conn, &opts);
 
-    char *data = bmalloc(READ_BUFFER_SIZE * sizeof(char));
-    size_t allocated = READ_BUFFER_SIZE;
-    int len = 0;
-    int chars_read;
-    do {
-        chars_read = fread(
-            data + allocated - READ_BUFFER_SIZE,
-            sizeof(char),
-            READ_BUFFER_SIZE,
-            file
-        );
+    } else if (ev == MG_EV_HTTP_MSG) {
+        struct mg_http_message *http_msg = (struct mg_http_message *) data;
+        struct mg_http_serve_opts opts = {};
 
-        data = brealloc(data, allocated + READ_BUFFER_SIZE);
-        allocated += READ_BUFFER_SIZE;
-
-        len += chars_read;
-    } while (chars_read > 0);
-
-    char *str = bstrdup_n(data, len);
-    return str;
-}
-
-static void get_path(char *header, int len, char *path) {
-    int pathstart = -1;
-    int pathlen = 0;
-    for (int i = 0; i < len; i++) {
-        if (header[i] == ' ') {
-            if (pathstart == -1) {
-                pathstart = i + 1;
-                continue;
-            } else {
-                pathlen = pathstart - i + 1;
-                break;
-            }
-        }
-
-        if (pathstart != -1) {
-            path[i - pathstart] = header[i];
+        if (mg_http_match_uri(http_msg, "/ws-port")) {
+            mg_http_reply(
+                conn,
+                200,
+                "Content-Type: text/plain\r\n",
+                server->ws_port
+            );
+        } else {
+            mg_http_serve_file(
+                conn,
+                http_msg,
+                obs_module_file("client/index.html"),
+                &opts
+            );
         }
     }
-
-    path[pathlen] = '\0';
 }
 
 const char http_header[] = "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n";
@@ -90,37 +76,7 @@ void* http_server_loop(void *arg) {
     struct http_server *server = arg;
 
     while (1) {
-        struct sockaddr_in client_address;
-        socklen_t client_address_len = sizeof(client_address);
-
-        int client_fd = accept(
-            server->socket_fd,
-            (struct sockaddr *) &client_address,
-            &client_address_len
-        );
-
-        if (client_fd < 0) {
-            obs_log(LOG_ERROR, "accept failed: %s", strerror(errno));
-            continue;
-        }
-
-        char header[1024];
-        int bytes_recvd = recv(client_fd, header, 1024, 0);
-        if (bytes_recvd < 0) {
-            obs_log(LOG_ERROR, "recv failed: %s", strerror(errno));
-        }
-
-        char path[1024];
-        get_path(header, bytes_recvd, path);
-
-        send(client_fd, http_header, (sizeof(http_header) - 1)/sizeof(char), 0);
-        if (strcmp(path, "/ws-port") == 0) {
-            send(client_fd, server->ws_port, strlen(server->ws_port)*sizeof(char), 0);
-        } else {
-            send(client_fd, server->html, strlen(server->html)*sizeof(char), 0);
-        }
-
-        shutdown(client_fd, SHUT_RDWR);
+        mg_mgr_poll(&server->mgr, 1000);
     }
 
     return NULL;
@@ -129,31 +85,12 @@ void* http_server_loop(void *arg) {
 struct http_server* http_server_create(int port) {
     struct http_server *server = malloc(sizeof(struct http_server));
 
-    server->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server->socket_fd < 0) {
-        obs_log(LOG_ERROR, "socket failed: %s", strerror(errno));
-        return NULL;
-    }
+    mg_mgr_init(&server->mgr);
+    server->mgr.userdata = server;
 
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(port);
-
-    setsockopt(server->socket_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
-    setsockopt(server->socket_fd, SOL_SOCKET, SO_REUSEPORT, &(int){1}, sizeof(int));
-
-    if (bind(server->socket_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
-        obs_log(LOG_ERROR, "bind failed: %s", strerror(errno));
-        return NULL;
-    }
-
-    if (listen(server->socket_fd, 2) < 0) {
-        obs_log(LOG_ERROR, "listen failed: %s", strerror(errno));
-        return NULL;
-    }
-
-    server->html = http_server_read_html();
+    char addr[32];
+    snprintf(addr, 32, "https://0.0.0.0:%d", port);
+    mg_http_listen(&server->mgr, addr, http_server_handler, NULL);
 
     server->ws_port = "-1";
 
@@ -178,7 +115,6 @@ void http_server_destroy(struct http_server **server_ptr) {
     // Stop the listen thread
     pthread_cancel(server->listen_thread);
 
-    bfree(server->html);
     free(server);
 
     *server_ptr = NULL;
